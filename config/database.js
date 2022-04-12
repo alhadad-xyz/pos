073e8b74046ba20f2391383cc76709b74a28894e
alhadad-xyz <alhadad.xyz@gmail.com>
2022-04-12 21:28:19 +0700

    edit database

--- config/database.js
+++ config/database.js
@@ -1,14 +1,7 @@
 const mongoose = require('mongoose')
 const {dbHost, dbPass, dbName, dbPort, dbUser} = require('./config')
-// const url = `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`
 const url = `mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}`
 mongoose.connect(url)
 const db = mongoose.connection
-
-if (db) {
-	console.log('OKAY')
-} else {
-	console.log('ERROR')
-}
 
 module.exports = db
